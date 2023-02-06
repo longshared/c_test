30cd5fd872661701e24087d5f985d71a9e90898e
longshared <781317395@qq.com>
2023-02-06 19:01:56 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "test1.h"
+int c;
+int main()
+{
+    // test();
+    std::cout << "this is a test test test" << std::endl;
+    std::cout << "this is a test test test" << std::endl;
+    std::cout << "this is a test test test" << std::endl;
+    int a,b;
+    a=1;
+    b=3;
+    c=2;
+    cout << "hello world" << endl;
+    a++;
+    b--;
+    cout<<"hello world " << endl;
+    return 0;
+}
---
+++ test1.cpp
@@ -0,0 +1,5 @@
+#include "test1.h"
+void test()
+{
+    std::cout << "this is a test log" << std::endl;
+}
